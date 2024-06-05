d90a70c16b3b498f47949d769e35c1e419cb7f6c
Anshul Mokhale <89470956+Anshul-Mokhale@users.noreply.github.com>
2024-06-05 14:58:10 +0530

    update 7

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: "http://mamta-medical.netlify.app/",
+
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || "http://mamta-medical.netlify.app",
     credentials: true
-}))
+};
 
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cors(corsOptions));
 
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
-//routes import
-import userRouter from './routes/user.routes.js'
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN); // Log to verify the value
 
+// routes import
+import userRouter from './routes/user.routes.js';
 
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
 
-export { app }
+export { app };
